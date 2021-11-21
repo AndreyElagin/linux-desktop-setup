01391d29f9238f5bd34e6730ecd4a357afd3cd61
Andrey Elagin <daddyingrave@gmail.com>
2021-11-21 18:38:30 +0300

    fix bluetooth?

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# install ansible
+# Ubuntu: install ansible
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 
+# Ubuntu: install curl and git
 sudo apt install curl git -y 
